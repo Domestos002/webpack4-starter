1995145c61181ab554ffb454e955572d394cc405
Domestos002 <absorber000@gmail.com>
2019-03-26 17:51:18 +0600

    min-shit final

--- webpack.config.js
+++ webpack.config.js
@@ -8,81 +8,77 @@ module.exports = function(env, argv){
   const environment = argv.mode;
   return {
     module: { 
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-     {
-        test: /\.twig$/,
-        use:['html-loader','twig-html-loader']
-      },
-      {
-        test: /\.(png|jpe?g)/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              name: "./img/[name].[ext]",
-              limit: 10000
-            }
-          },
-          {
-            loader: "img-loader"
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
-      {
-          test: /\.sass$/,
+        },
+       {
+          test: /\.twig$/,
+          use:['html-loader','twig-html-loader']
+        },
+        {
+          test: /\.(png|jpe?g)/i,
           use: [
-            environment === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
             {
-              loader: 'css-loader',
+              loader: "url-loader",
               options: {
-                sourceMap: true
-              }
-            },      
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: () => {
-                  environment === 'production' ? [autoprefixer, cssnano] : [autoprefixer]
-                  return [autoprefixer, cssnano]
-                },
-                sourceMap: true
+                name: "./img/[name].[ext]",
+                limit: 10000
               }
             },
             {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
+              loader: "img-loader"
             }
           ]
-      }
+        },
+        {
+            test: /\.sass$/,
+            use: [
+              environment === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },      
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins: () => {
+                    environment === 'production' ? [autoprefixer, cssnano] : [autoprefixer]
+                    return [autoprefixer, cssnano]
+                  },
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+        }
+      ],
+    },
+    stats: 'errors-only',
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.DEBUG': JSON.stringify(process.env.DEBUG)
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.twig",
+        filename: "index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
     ]
-  },
-  stats: {
-    warnings: false,
-    children: false
-  },
-  // devServer: {
-  //   contentBase: './dist', 
-  //   hot: true
-  // },   
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.twig",
-      filename: "index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    // new webpack.HotModuleReplacementPlugin()
-  ]
-}
+  }
 };
